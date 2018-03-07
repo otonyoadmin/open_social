abc7e86add810eca11cdc1381d82a62729e5ae3d
brhane <godseeshears@gmail.com>
2018-03-07 14:41:24 +0100

    Adding Name and username to the user export

--- modules/social_features/social_user_export/src/Plugin/Action/ExportUser.php
+++ modules/social_features/social_user_export/src/Plugin/Action/ExportUser.php
@@ -32,7 +32,7 @@ class ExportUser extends ActionBase implements ContainerFactoryPluginInterface {
    *
    * @var \Drupal\Core\Session\AccountInterface
    */
-  protected $current_user;
+  protected $currentUser;
 
   /**
    * Apply for all.
@@ -48,22 +48,21 @@ class ExportUser extends ActionBase implements ContainerFactoryPluginInterface {
    */
   protected $query = [];
 
-  /**
-   * Constructs a ExportUser object.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin ID for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\user\PrivateTempStoreFactory $temp_store_factory
-   *   The tempstore factory.
-   * @param \Drupal\Core\Session\AccountInterface $current_user
-   *   Current user.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, PrivateTempStoreFactory $temp_store_factory, AccountInterface $current_user) {
-    $this->current_user = $current_user;
+    /**
+     * Constructs a ExportUser object.
+     *
+     * @param array $configuration
+     *   A configuration array containing information about the plugin instance.
+     * @param string $plugin_id
+     *   The plugin ID for the plugin instance.
+     * @param mixed $plugin_definition
+     *   The plugin implementation definition.
+     * @param \Drupal\user\PrivateTempStoreFactory $temp_store_factory
+     *   The tempstore factory.
+     * @param AccountInterface $currentUser
+     */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, PrivateTempStoreFactory $temp_store_factory, AccountInterface $currentUser) {
+    $this->currentUser = $currentUser;
     $this->tempStoreFactory = $temp_store_factory;
 
     parent::__construct($configuration, $plugin_id, $plugin_definition);
